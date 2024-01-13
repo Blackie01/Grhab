67c6f110501506d55b3ef6dbd3dca2ab4f537dba
Blackie01 <onioludamola@gmail.com>
2024-01-13 23:48:55 +0100

    rewrote code

--- content.js
+++ content.js
@@ -1,8 +1,17 @@
 
-console.log('Content script loaded');
+// console.log('Content script loaded');
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.action === 'screenshotCaptured') {
-    console.log('received screenshot url', request.screenshotUrl);
-  }
-});
+// setTimeout(function() {
+// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+//   if (request.action === 'screenshotCaptured') {
+//     console.log('received screenshot url', request.screenshotUrl);
+//   }
+// });
+// }, 3000);
+
+
+// const port = chrome.runtime.connect({ name: 'content-script' });
+// port.postMessage({ action: 'captureScreenshot' });
+// port.onMessage.addListener(function(response) {
+//   console.log('Received response:', response);
+// });
--- popup.js
+++ popup.js
@@ -1,67 +1,108 @@
-// console.log("Popup script loaded");
+// // console.log("Popup script loaded");
 
-// document.getElementById("capture").addEventListener("click", function () {
-//   console.log("this runs");
+// // document.getElementById("capture").addEventListener("click", function () {
+// //   console.log("this runs");
 
-//   try {
-//     chrome.runtime.sendMessage({ action: "captureScreenshot" });
-//   } catch (error) {
-//     console.error(" here is error point 1", error);
-//   }
-// });
+// //   try {
+// //     chrome.runtime.sendMessage({ action: "captureScreenshot" });
+// //   } catch (error) {
+// //     console.error(" here is error point 1", error);
+// //   }
+// // });
 
-// document.getElementById("download").addEventListener("click", function () {
-//   try {
-//     chrome.runtime.sendMessage({ action: "createPDF" });
-//   } catch (error) {
-//     console.error(error);
-//   }
-// });
+// // document.getElementById("download").addEventListener("click", function () {
+// //   try {
+// //     chrome.runtime.sendMessage({ action: "createPDF" });
+// //   } catch (error) {
+// //     console.error(error);
+// //   }
+// // });
 
-// "content_scripts": [
-//     {
-//       "matches": ["<all_urls>"],
-//       "js": [
-//         "content.js"
-//       ]
+// // "content_scripts": [
+// //     {
+// //       "matches": ["<all_urls>"],
+// //       "js": [
+// //         "content.js"
+// //       ]
+// //     }
+// //   ]
+
+
+// //////////////////////////////////////////////////////////////
+
+
+// document.getElementById("capture")?.addEventListener("click", function () {
+
+//   document.getElementById("capture").style.backgroundColor = "red";
+
+//   chrome.runtime.sendMessage({
+//     action: "captureScreenshot",
+//     function(response) {
+//       document.getElementById("capture").innerText = "Captured"
 //     }
-//   ]
+//   });
 
-console.log("Popup script loaded");
+//   document.getElementById("capture").innerText = "Capturing...";
+//   alert('there was a click')
+
+// });
+
+// document.getElementById("download")?.addEventListener("click", function () {
+  
+//   console.log("Download button clicked");
+
+//   chrome.runtime.sendMessage({
+//     action: "createPDF",
+//     function(response) {
+//       document.getElementById("download").innerText = "Done"
+//     }
+//   });
+
+//   document.getElementById("download").innerText = "Downloading...";
+// });
+
+
+// //
+// chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+//   if (message.action === "screenshotCaptured") {
+//     // Show capture success message
+//     document.getElementById("capture").innerText = "Screenshot Captured";
+//   } else if (message.action === "screenshotError") {
+//     // Show capture error message
+//     alert(message.error);
+//   } else if (message.action === "pdfDownloadComplete") {
+//     // Show download completion message
+//     document.getElementById("download").innerText = "Download Complete";
+//   } else if (message.action === "pdfError") {
+//     // Show download error message
+//     alert(message.error);
+//   }
+// });
+
+
+
+
+
 
 document.getElementById("capture")?.addEventListener("click", function () {
-  document.getElementById("capture").style.backgroundColor = "red";
-  chrome.runtime.sendMessage({
-    action: "captureScreenshot",
+  chrome.runtime.sendMessage({ action: "captureScreenshot" }, function(response) {
+    document.getElementById("capture").innerText = "Captured";
   });
-
-  //
   document.getElementById("capture").innerText = "Capturing...";
+  console.log('does this run')
 });
 
 document.getElementById("download")?.addEventListener("click", function () {
-  console.log("Download button clicked");
-  chrome.runtime.sendMessage({
-    action: "createPDF",
+  chrome.runtime.sendMessage({ action: "createPDF" }, function(response) {
+    document.getElementById("download").innerText = "Done";
   });
-
-  //
   document.getElementById("download").innerText = "Downloading...";
 });
 
-//
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  if (message.action === "screenshotCaptured") {
-    // Show capture success message
-    document.getElementById("capture").innerText = "Screenshot Captured";
-  } else if (message.action === "screenshotError") {
-    // Show capture error message
+  if (message.action === "screenshotError") {
     alert(message.error);
-  } else if (message.action === "pdfDownloadComplete") {
-    // Show download completion message
-    document.getElementById("download").innerText = "Download Complete";
   } else if (message.action === "pdfError") {
-    // Show download error message
     alert(message.error);
   }
 });
